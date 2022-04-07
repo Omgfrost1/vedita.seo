461cfaae091110dcb2521d9ccf21ff4c2ebaaca0
Omgfrost1 <V-hutarcov@mail.com>
2022-04-07 12:19:34 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+
+   $('.cases__slider').slick({
+    dots: false,
+    arrows: true,
+    prevArrow: '<button type = "button" class= "cases__prev"><img src="images/cases/arrow__left.svg" alt="left arrow"> </button>',
+    nextArrow: '<button type = "button" class= "cases__next"><img src="images/cases/arrow__right.svg" alt="right arrow"> </button>',
+   
+  });
+  
+});
+
+
